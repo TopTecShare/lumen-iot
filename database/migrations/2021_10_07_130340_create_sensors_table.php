e1958bda2e4d58d0305940b4bd722473be8d7f85
netmaniac <rugowski@nhw.pl>
2021-10-07 21:58:48 +0200

    first model

---
+++ database/migrations/2021_10_07_130340_create_sensors_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSensorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sensors', function (Blueprint $table) {
+            $table->charset = 'utf8mb4';
+            $table->collation = 'utf8mb4_unicode_ci';
+
+            $table->id();
+            $table->timestamps();
+            $table->string('uuid', 36)
+                ->default('')
+                ->nullable(false);
+            //SensorID but would mess with naming conventions
+            $table->string('sensor_number', 100)
+                ->default('')
+                ->nullable(false);
+
+            $table->unique('uuid');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sensors');
+    }
+}
